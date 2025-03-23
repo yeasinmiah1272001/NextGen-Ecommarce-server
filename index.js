ebab8545e7e963d20b16bf9140348dcb1cec1793
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-03-23 21:55:57 +0600

    single api complete

--- index.js
+++ index.js
@@ -102,6 +102,13 @@ async function run() {
       const result = await productCollection.find().toArray();
       res.send(result);
     });
+    app.get("/product/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
